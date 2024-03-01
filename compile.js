29f0d4d145f116c565cb72d484a27d67191c6d74
vishal18713 <lcs2023052@iiitl.ac.in>
2024-03-01 21:02:46 +0530

    initial setup

---
+++ compile.js
@@ -0,0 +1,5 @@
+const path = require('path');
+//this is for calculating the right path of the file in the system
+const fs = require('fs');
+//this is for reading the file and getting its content
+const fileToCompile = "BodyMap.sol";
---
+++ contracts/BodyMap.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.19;
+
+contract BodyMap{
+    bytes32 private passwordHash;
+    string public basicBodyMap;
+    string public tailorBodyMap;
+    constructor (string memory passwordClearText)
+    {
+        
+        passwordHash = keccak256(abi.encodePacked(passwordClearText));
+    }
+    function setBodyMaps(string memory passwordClearText,string memory newBasicBodyMap,string memory newTailorBodyMap)public {
+        bytes32 givenPasswordHash = keccak256(abi.encodePacked(passwordClearText));
+        if(givenPasswordHash == passwordHash){
+            basicBodyMap = newBasicBodyMap;
+            tailorBodyMap = newTailorBodyMap;
+
+        }
+        
+
+    }
+}
