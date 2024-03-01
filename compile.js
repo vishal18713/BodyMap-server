a24f621e236fc4bd833081508b60ffb5319cf8eb
vishal18713 <lcs2023052@iiitl.ac.in>
2024-03-01 21:25:07 +0530

    Update compile.js with correct contract name and path

--- compile.js
+++ compile.js
@@ -2,4 +2,26 @@ const path = require('path');
 //this is for calculating the right path of the file in the system
 const fs = require('fs');
 //this is for reading the file and getting its content
-const fileToCompile = "BodyMap.sol";
+const fileToCompileName = "BodyMap.sol";
+//contract name
+const contractPath = path.resolve(__dirname, 'contracts', contractToCompileName);
+//this for no avid issues its correct path of the file
+const contractSource = fs.readFileSync(contractPath,'utf-8');
+const solc = require('solc');
+var input = 
+{
+    language : 'Solidity',
+    sources : {[contractToCompileName]:{content:contractSource}},
+    setting : {outputSelection:{'*':{'*':['*']}}}
+
+
+}
+
+const compiledContracts = JSON.parse(solc.compile(JSON.stringify(input)));
+console.log(compiledContracts);
+const compiledContract =compiledContracts.contracts[contractToCompileName]["BodyMap"];
+console.log(compiledContract);
+module.exports = {compiledContract};
+//this is for exporting the compiled contract to use it in the other files
+
+
