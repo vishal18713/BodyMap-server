3e3df157aef8bc707f0b37a94791ff4bc518febf
vishal18713 <lcs2023052@iiitl.ac.in>
2024-03-01 21:43:48 +0530

    Add dependencies and import statements to BodyMap.test.js

---
+++ test/BodyMap.test.js
@@ -0,0 +1,6 @@
+const {compiledContract} = require('../compile');
+//this line is for the to navigate up one directory level from the current directory
+const assert = require('assert');
+//this is used to inport trh built in assert module provides a simple set of assertion tests that can be used to test the functionality of your code
+const ganache = require('ganache-cli');
+// 
